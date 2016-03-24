0c27bc68bd76ebf4d111bc0ab13fea4c679a4bef
DarkGL <gitzzz14@gmail.com>
2016-03-24 12:41:14 +0100

    LSO data block

--- plugin/script.js
+++ plugin/script.js
@@ -1,11 +1,20 @@
 function loadFunction(){
 	var allScripts = document.getElementsByTagName( 'script' );
 	
-	for( currentPosition = 0; currentPosition < allScripts.length; currentPosition++ ) {
-		if( allScripts[currentPosition].src.indexOf( "evercookie" ) > -1 ){
-			console.log( allScripts[currentPosition].src )
+	for( var currentPosition = 0; currentPosition < allScripts.length; currentPosition++ ) {
+		if( allScripts[currentPosition].src.indexOf( 'evercookie' ) > -1 ){
 			allScripts[currentPosition].parentNode.removeChild(allScripts[currentPosition]);
 		}
+	}
+	
+	var swfObject = document.getElementById( 'swfcontainer' );
+	
+	if( swfObject ){
+		swfObject.parentNode.removeChild(swfObject);
+	}
+	
+	window.Evercookie = function( options ){
+		
 	}
 }
 
